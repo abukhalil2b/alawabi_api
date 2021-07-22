e04d756a43199ef9028c3b0985c91118cbffb799
abukhalil2b <abukhalil2b@gmail.com>
2021-07-22 06:40:11 +0400

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,6 @@
         <a class="shadow-xl px-3" href="{{route('question.index')}}">قائمة الأسئلة</a>
         <a class="shadow-xl px-3" href="{{route('student.index')}}">قائمة الطلاب</a>
         <a class="shadow-xl px-3" href="{{route('answer.lot')}}">القرعة</a>
-        <a class="shadow-xl px-3" href="{{route('winner.create')}}">الفائزون في السحب</a>
         <a class="shadow-xl px-3" href="{{route('sponser.index')}}">الداعمون</a>
     </x-slot>
 </x-app-layout>
